cc1cc3fe4fbb88fba15608fc7f1bdef06d2556ba
Diego <diecolgar@gmail.com>
2023-02-13 14:40:10 +0100

    Web Landing

--- src/App.js
+++ src/App.js
@@ -8,24 +8,26 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <div className="nav-logo">
-            <div className="logo">
+        <div className="nav-container">
+          <div className="nav-logo">
+              <div className="logo">
 
-            </div>
-        </div>
-        <ul className="nav-links">
-          <li className="nav-AI">
-            <Link to="/">AI</Link>
-          </li>
-          <li className="separator">
+              </div>
+          </div>
+          <ul className="nav-links">
+            <li className="nav-AI">
+              <Link to="/">AI</Link>
+            </li>
+            <li className="separator">
 
-          </li>
-          <li className="nav-3D">
-            <Link to="/about">3D</Link>
-          </li>
-        </ul>
-        <div className="nav-dummy">
+            </li>
+            <li className="nav-3D">
+              <Link to="/about">3D</Link>
+            </li>
+          </ul>
+          <div className="nav-dummy">
 
+          </div>
         </div>
       </nav>
 
